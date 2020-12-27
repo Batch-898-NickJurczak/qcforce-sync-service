8e1b6c0a841619606b66d1e607d0c17fd254635e
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2020-12-27 14:52:35 -0500

    Set up structure for Survey tasks

---
+++ SyncService/src/main/java/com/revature/service/SurveyServiceImpl.java
@@ -0,0 +1,20 @@
+package com.revature.service;
+
+import com.revature.models.SurveyForm;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * A standard implementation of the {@link SurveyService} interface for handling
+ * all the business code related to {@link SurveyForm} objects.
+ * 
+ * @author Conner,
+ * @author Michael M,
+ * @author Michael Z,
+ * @author Prativa
+ */
+@Service
+public class SurveyServiceImpl {
+
+    
+}
