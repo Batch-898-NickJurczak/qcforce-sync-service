c583f1365aae3898d1bc6771c8ef044c2f5b436b
Chris Breniser <chrisbreniser@gmail.com>
2020-12-28 15:09:06 -0800

    Init impl of get all survey controller and service.

--- SyncService/src/main/java/com/revature/controllers/SurveyController.java
+++ SyncService/src/main/java/com/revature/controllers/SurveyController.java
@@ -1,12 +1,18 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.dto.SurveyFormDto;
 import com.revature.models.SurveyForm;
+import com.revature.service.SurveyService;
 
 import reactor.core.publisher.Flux;
 
@@ -23,12 +29,39 @@ import reactor.core.publisher.Flux;
 public class SurveyController {
 	
 	/**
+	 * The service we use to interact with our business logic. 
+	 */
+	private SurveyService surveyService;
+
+	/**
+	 * The setter for the {@link SurveyService} that will be autowired by default
+	 * @param surveyService the surveyService to set 
+	 */
+	@Autowired
+	public void setSurveyService(SurveyService surveyService) {
+		this.surveyService = surveyService;
+	}
+	
+	/**
 	 * Retrieves a {@link List} of all {@link SurveyForm}s.
-	 * @return A promise for a {@link List} of {@link SurveyForm}s.
+	 * @return A promise of {@link SurveyForm}s.
 	 */
 	@GetMapping("survey")
-	public Flux<List<SurveyForm>> getAllSurveyForms(){
-		return null;
+	public Flux<SurveyFormDto> getAllSurveyForms(HttpServletResponse sr){
+		
+		List<SurveyForm> surveyForms = surveyService.getAllSurveyForms();
+		List<SurveyFormDto> surveyFormsDto = new ArrayList<>();
+		
+		for (SurveyForm sf : surveyForms) {
+			surveyFormsDto.add(new SurveyFormDto(sf));
+		}
+		
+		sr.setStatus(200);
+		
+		// Fill Flux data with surveyFormsDto list content if it exists. 
+		Flux<SurveyFormDto> data = Flux.fromIterable(surveyFormsDto);
+		
+		return data;
 	};
     
 }
--- SyncService/src/main/java/com/revature/service/SurveyServiceImpl.java
+++ SyncService/src/main/java/com/revature/service/SurveyServiceImpl.java
@@ -1,9 +1,11 @@
 package com.revature.service;
 
 import com.revature.models.SurveyForm;
+import com.revature.repo.SurveyRepo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,14 +21,26 @@ import org.springframework.stereotype.Service;
 public class SurveyServiceImpl implements SurveyService{
 	
 	/**
+	 * The instance of a dao that we will use to interact with our database
+	 */
+	private SurveyRepo surveyRepo;
+	
+	/**
+	 * The setter for the {@link SurveyRepo} that will be autowired by default
+	 * @param surveyRepo the surveyRepo to set
+	 */
+	@Autowired
+	public void setSurveyRepo(SurveyRepo surveyRepo) {
+		this.surveyRepo = surveyRepo;
+	}
+
+	/**
 	 * Retrieves a {@link List} of all {@link SurveyForm}s stored by the repo.
 	 * @return {@link List} of {@link SurveyForm}s.
 	 */
 	@Override
 	public List<SurveyForm> getAllSurveyForms() {
-		// TODO Auto-generated method stub
-		return null;
+		return surveyRepo.findAll();
 	}
-
-    
+	
 }
