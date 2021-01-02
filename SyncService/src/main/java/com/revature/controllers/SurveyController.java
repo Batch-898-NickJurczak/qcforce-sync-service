28b35436dcd51da8de78b9ede91b68b18f9f0981
JoeDKoal94 <joedkoal94@gmail.com>
2021-01-02 17:05:33 -0600

    Updated Testing

--- SyncService/src/main/java/com/revature/controllers/SurveyController.java
+++ SyncService/src/main/java/com/revature/controllers/SurveyController.java
@@ -9,23 +9,28 @@ import javax.ws.rs.PathParam;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.service.AuthServiceImpl;
 import com.revature.service.SurveyService;
 import com.revature.dto.SurveyFormDto;
+import com.revature.models.SurveyForm;
 
 import antlr.collections.List;
 import io.jsonwebtoken.Claims;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
+@RestController
+@CrossOrigin
 public class SurveyController {
 	
 	/**
@@ -56,26 +61,27 @@ public class SurveyController {
 	 * @return 
 	 */
 	@GetMapping("/survey/{token}")
-	public Flux<ArrayList<Object>> getSurveyById(@PathVariable("token") String token) {
+	@ResponseStatus(code = HttpStatus.OK)
+	public Flux<Object> getSurveyById(@PathVariable("token") String token) {
 		Map<String, Object> claim = authService.decodeJWT(token);
-		ArrayList<Object> object = new ArrayList<Object>();
+		ArrayList<Object> response = new ArrayList<Object>();
 		Date date = new Date(System.currentTimeMillis());
 		
 		if(date.compareTo((Date)claim.get("exp")) == 1) {
-			object.add("expired");
-			object.add(null);
+			response.add("expired");
 		} else if(claim.get("iat") == null) {
-			object.add("failure");
-			object.add(null);
+			response.add("failure");
 		} else if (claim.get("surveySubId") != null) {
-			object.add("completed");
-			object.add(null);
+			response.add("completed");
 		} else {
-			object.add("success");
-			object.add(new SurveyFormDto(surveyService.getSurvey((int)claim.get("surveyId"))));
+			response.add("success");
+			SurveyForm survey = (surveyService.getSurvey((int)claim.get("surveyId")));
+			response.add(new SurveyFormDto(survey));
 		}
 		
-		return Flux.just(object);
+		Flux<Object> data = Flux.fromIterable(response);
+		
+		return data;
 	}
 }
 
