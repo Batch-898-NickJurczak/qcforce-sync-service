e9988877357c339bed55410cd2e4d0eebad5636e
PrativaG <c7.paru@gamil.com>
2021-01-02 18:40:52 -0800

    Implemented tests for authenticating token

--- SyncService/src/main/java/com/revature/service/AuthServiceImpl.java
+++ SyncService/src/main/java/com/revature/service/AuthServiceImpl.java
@@ -1,26 +1,39 @@
 package com.revature.service;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
-
+@Service
 public class AuthServiceImpl {
 	
-
+	/*
+	 * This string is the key required for decoding JWT
+	 * */
 	private static String secretKey;
 	
-public AuthServiceImpl(@Value("${token_secret}") String secretKey) {
+	/*
+	 * Assigns the secretKey required to decode JWT. 
+	 * Needs to be set as environment variables.
+	 * */
+	public AuthServiceImpl(@Value("${token_secret}") String secretKey) {
 		AuthServiceImpl.secretKey = secretKey;
 	}
 	
-	public HashMap<String, Object> decodeJWT(String jwt) {
-
+	/*
+	 * Decodes the jwt received from the controller.
+	 * @return Map Returns the {@link Map} of jwt claims.
+	 * @param jwt String of token.
+	 * */
+	public Map<String, Object> decodeJWT(String jwt) {
+		
 	    return null;
 	}
 	
