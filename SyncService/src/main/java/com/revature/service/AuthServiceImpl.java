75e032a532531e030f1e207edf253505395cc03e
JoeDKoal94 <joedkoal94@gmail.com>
2021-01-03 15:56:39 -0600

    Updated Test for approval

--- SyncService/src/main/java/com/revature/service/AuthServiceImpl.java
+++ SyncService/src/main/java/com/revature/service/AuthServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 
 @Service
 public class AuthServiceImpl {
@@ -17,6 +18,8 @@ public class AuthServiceImpl {
 	 * This string is the key required for decoding JWT
 	 * */
 	private static String secretKey;
+	
+	private Map<String, Object> claim;
 	
 	/*
 	 * Assigns the secretKey required to decode JWT. 
@@ -27,12 +30,28 @@ public class AuthServiceImpl {
 	}
 	
 	/*
-	 * Decodes the jwt received from the controller.
+	 * After Decoding the jwt received from the controller, retrieves the Claims of the token to be used by the system.
 	 * @return Map Returns the {@link Map} of jwt claims.
+	 * Map contents (keys):
+	 *  - "iat" - Issued time of token creation.
+	 *  - "exp" - Expiration of token, set to be 15 mins after creation.
+	 *  - "surveyId" - ID of survey template being sent out.
+	 *  - "surveySubId" - ID of submission of the survey. Will populate only when the user already have taken it.
+	 *  - "batchId" - ID of batch who are taking the survey. 
 	 * @param jwt String of token.
 	 * */
-	public Map<String, Object> decodeJWT(String jwt) {
-	    return null;
+	public Map<String, Object> getClaim() { 
+	    return claim;
+	}
+	
+	
+	/*
+	 * Verifies the jwt token taken inside the URL. If authenticated will store the claim value to be retrieved by getClaims().
+	 * @param String  JWT String token to be verified.
+	 * @return boolean Returns true if verified, stores the claims decoded into a Map object to be retrieved. Returns false otherwise.
+	 */
+	public boolean verifyJWT(String jwt) {
+		return false;
 	}
 	
 	
--- SyncService/src/test/java/com/revature/service/AuthServiceImplTest.java
+++ SyncService/src/test/java/com/revature/service/AuthServiceImplTest.java
@@ -23,11 +23,11 @@ class AuthServiceImplTest {
 	private AuthServiceImpl auth = new AuthServiceImpl("secret");
 	
 	/*
-	 * Tests decodeJWT method of {@link AuthServiceImpl}
-	 * Test is successful if decoded token has expected value.
+	 * Tests verifyJWT method of {@link AuthServiceImpl}
+	 * Test is successful if decoded token is verified.
 	 * */
 	@Test
-	void decodeJWTTest_Authenticated() {
+	void verifyJWTTest_Authenticated() {
 		
 		token = Jwts.builder().setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 15)))
@@ -37,18 +37,18 @@ class AuthServiceImplTest {
 				.signWith(SignatureAlgorithm.HS256, "secret")
 				.compact();
 
-		Map<String, Object> returned = auth.decodeJWT(token);
+		boolean returned = auth.verifyJWT(token);
 		
-		assertEquals(1, returned.get("surveyId"), "decodeJWTTest_Authenticated couldn't authenticate the token");
+		assertTrue(returned);
 			
 		}
 	
 	/*
-	 * Tests decodeJWT method of {@link AuthServiceImpl}
-	 * Returns true if SignatureException is thrown.
+	 * Tests verifyJWT method of {@link AuthServiceImpl}
+	 * Returns false if SignatureException is thrown and user is unauthenticated.
 	 * */
 	@Test()
-	void decodeJWTTest_NotAuthenticated() {
+	void verifyJWTTest_NotAuthenticated() {
 				
 		token = Jwts.builder().setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 15)))
@@ -58,17 +58,32 @@ class AuthServiceImplTest {
 				.signWith(SignatureAlgorithm.HS256, "notSecret")
 				.compact();
 		
-		boolean thrown = false;
-		
-		try {
-			
-			Map<String, Object> returned = auth.decodeJWT(token);
-			
-		} catch (SignatureException e) {
-			thrown = true;
-		}
-		
-		assertTrue(thrown);		
+		boolean returned = auth.verifyJWT(token);
+		assertFalse(returned);		
 		}
 
+	
+	/*
+	 * Tests getClaim method of of {@link AuthServiceImpl}
+	 * Returns the Claims of the token after verifying the user.
+	 */
+	@Test
+	void getClaimTest_CheckItems() {
+		
+		token = Jwts.builder().setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 15)))
+				.claim("surveyId", 1)
+				.claim("batchId", "1")
+				.claim("surveySubId", 1)
+				.signWith(SignatureAlgorithm.HS256, "secret")
+				.compact()
+				;
+
+		boolean check = auth.verifyJWT(token);
+		Map<String, Object> returned = auth.getClaim();
+		
+		
+		assertEquals(1, returned.get("surveyId"), "getClaimTest_CheckItems have encountered an error in fetching info from the claim");
+			
+		}
 }
