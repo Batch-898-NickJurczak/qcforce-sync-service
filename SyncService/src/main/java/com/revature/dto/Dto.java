4daedbc26fdebebd31502bd57335d3987794f079
Zachary Leonardo <zachleonardo15@gmail.com>
2021-01-04 20:29:05 -0500

    updated dto interface to match dev branch

--- SyncService/src/main/java/com/revature/dto/Dto.java
+++ SyncService/src/main/java/com/revature/dto/Dto.java
@@ -1,8 +1,24 @@
+  
 package com.revature.dto;
 
+/**
+ * A data transfer object, designed to facilitate the transformation 
+ * between external and internal data in a Java program. This functional
+ * interface contains a method that converts itself into its Pojo equivalent.
+ * Generic type should be the Pojo this Dto relates to.
+ * @author Chris,
+ * @author Conner,
+ * @author Michael M,
+ * @author Michael Z,
+ * @author Prativa,
+ * @author Vincent
+ */
 public interface Dto<T> {
-    
-    public T toPojo();
-    
-}
+	
+	/**
+	 * Converts this Dto object into its Pojo equivalent.
+	 * @return Java Pojo
+	 */
+	public T toPojo();
 
+}
